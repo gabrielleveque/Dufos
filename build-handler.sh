edc51786bfda8c685db814c8d4365cd451404003
Gabriel Leveque <gabriel.leveque@epitech.eu>
2025-11-14 13:53:22 +0100

    fix (script): changed build script to be less specific

--- build-handler.sh
+++ build-handler.sh
@@ -2,6 +2,7 @@
 
 PATH_TO_COMMON="common"
 PATH_TO_NETWORK="network"
+PATH_TO_SERVER="server"
 
 OPT="$1"
 
@@ -12,21 +13,15 @@ if [ -z "$OPT" ] || [ "$OPT" == "-h" ] || [ "$OPT" == "--help" ]; then
     echo "\t - opt    The operation the handler should do (build or clean)."
 fi
 
-if [ "$OPT" == "common" ]; then
-    cmake -B build/${OPT} -S ${PATH_TO_COMMON} -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=${OPT}
-    cmake --build build/${OPT} --parallel
-    exit 0
-fi
-
-if [ "$OPT" == "network" ]; then
-    cmake -B build/${OPT} -S ${PATH_TO_NETWORK} -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=${OPT}
-    cmake --build build/${OPT} --parallel
-    exit 0
-fi
-
 if [ "$OPT" == "clean" ]; then
     rm -rf build/
-    rm -rf ${PATH_TO_COMMON}/build
     find . -type f -name '*.a' -delete
+    find . -type f -name 'dufos_server' -delete -or -name 'dufos_client' -delete
+    exit 0
+fi
+
+if [ ! -z "$OPT" ]; then
+    cmake -B build/${OPT} -S ${OPT} -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake -DCMAKE_BUILD_TYPE=${OPT}
+    cmake --build build/${OPT} --parallel
     exit 0
 fi
